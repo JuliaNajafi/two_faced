7752b33f85530a4df773db4acc15e0abfac44ae5
Julia Najafi <Julia.Najafi@gmail.com>
2016-07-05 16:31:13 -0400

    added headline + auto parse title and image from url

--- app/controllers/headlines_controller.rb
+++ app/controllers/headlines_controller.rb
@@ -3,4 +3,16 @@ class HeadlinesController < ApplicationController
     @issue = Issue.find(params[:issue_id])
     @headline = @issue.headlines.new
   end
+  def create
+    @issue = Issue.find(params[:issue_id])
+    @headline = @issue.headlines.create!(headline_params)
+
+    redirect_to issue_path(@issue)
+  end
+
+
+  private
+  def headline_params
+    params.require(:headline).permit(:url, :issue_id, :affiliation)
+  end
 end

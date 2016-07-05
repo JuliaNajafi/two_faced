404933a652c230ca9a973bb50e5a362f1f2f508f
Julia Najafi <Julia.Najafi@gmail.com>
2016-07-05 15:33:45 -0400

    add headline functionality

--- app/controllers/headlines_controller.rb
+++ app/controllers/headlines_controller.rb
@@ -1,2 +1,6 @@
 class HeadlinesController < ApplicationController
+  def new
+    @issue = Issue.find(params[:issue_id])
+    @headline = @issue.headlines.new
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
+
+
 module ApplicationHelper
+  def parse_title_and_img url
+    res = HTTParty.get(url)
+    page = Nokogiri::HTML(res)
+    result = Hash.new
+    result[:title] = page.css("title").text
+    result[:image] = page.css('[property="og:image"]').first['content']
+    return result
+    # httparty Call
+    # parse that response into html w/ NOKOgiri
+    # grab the title from the html
+    # grab the image_url from the html
+    # return an a hash with both things
+  end
 end

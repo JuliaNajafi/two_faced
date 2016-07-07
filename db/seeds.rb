46751bafc554a908b8d90223c7aab1e7eebdb1be
Julia Najafi <Julia.Najafi@gmail.com>
2016-07-07 12:33:43 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,10 @@ brexit_right = Headline.create([
   {title:"Brexit: Sovereign Kingdom or Little England?", description: "Brexit's Cost: Will the United Kingdom & EU Survive the British Brexit Vote", source: "The National Review", url:"http://www.nationalreview.com/article/437361/brexit-cost-will-united-kingdom-eu-survive-british-brexit-vote", affiliation: "right", issue: brexit},
   {title:"Brexit and the Global Elite", description:"Chalk up another loss for the global elite after Brexit loss", source: "Fox News", url: "http://www.foxnews.com/opinion/2016/06/30/chalk-up-another-loss-for-global-elite-after-brexit-loss.html", affiliation: "right", issue: brexit}
 ])
+
+abortion = Issue.create(title: "Reproductive Rights", description: "pro-choice or that other side?", img_url: "http://i.huffpost.com/gen/2555832/images/o-ABORTION-CLINIC-facebook.jpg")
+
+headlines_abortion = Headline.create([
+  {url:"http://www.nytimes.com/2016/06/28/us/supreme-court-texas-abortion.html", affiliation: "left", issue: abortion},
+  {url: "http://www.foxnews.com/politics/2016/06/27/supreme-court-strikes-down-texas-abortion-law.html", affiliation: "right", issue: abortion}
+])
